0ed53351dca5445c579a378eb7c86872e918a44c
Khamz <khamzzaty@mail.ru>
2023-07-10 16:58:36 +0300

    добавил модель публикаций

---
+++ models/Publication.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const publicationSchema = mongoose.Schema({
+    header: String,
+    image:String,
+    user:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:"User"
+    },
+    desc:String,
+    categorie:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:"Categories"
+    }
+})
+
+const Publication = mongoose.model("Publication", publicationSchema)
+
+module.exports = Publication
