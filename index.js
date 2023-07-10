2d6002f3cc5c96c6cd4e59726742a66456ce83ab
Djamalayl <djamalayl32@mail.ru>
2023-07-10 16:07:48 +0300

    dleted route

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-app.use(require("./routes/users.route"));
-
 mongoose
   .connect(process.env.MONGO_SERVER)
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
