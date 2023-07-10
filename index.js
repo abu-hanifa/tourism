5e5a94f8ee5bebde82f13707614c49d02d3c80fa
Djamalayl <djamalayl32@mail.ru>
2023-07-10 15:31:36 +0300

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.use(require("./routes/users.route"));
+
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => console.log("Успешно соединились с сервером MongoDB"))
+  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Сервер запущен успешно");
+});
