8bc8d62b16d142705d153c5386b34835a4941412
Khamz <khamzzaty@mail.ru>
2023-07-11 13:32:58 +0300

    добавил мультер мидлвэры

--- routes/publications.route.js
+++ routes/publications.route.js
@@ -1,12 +1,13 @@
 const Router  = require ('express');
 const {publicationController} = require('../controllers/publications.controller');
 const authMiddleware  = require('../models../middlewares/auth.middleware.js');
+const imgMW = require('../models/middlewares/multer.middleware')
 
 const router  = Router();
 router.get('/publications',authMiddleware, publicationController.getPublcation );
 router.get('/publicationsById/:id',authMiddleware, publicationController.getPublicationById)
-router.post('/addPublications',  authMiddleware,publicationController.postPublications);
-router.patch('/changePublications/:id', authMiddleware, publicationController.patchPublications);
+router.post('/addPublications', imgMW.single('img'), authMiddleware,publicationController.postPublications);
+router.patch('/changePublications/:id',imgMW.single('img'), authMiddleware, publicationController.patchPublications);
 router.delete('/deletePublications/:id', authMiddleware, publicationController.deletePublication);
 
 module.exports  = router
